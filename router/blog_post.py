94ee185224186c6b7b7952b4d9f7e71874349880
Hossein <hossienbehzadi1381@gmail.com>
2023-12-18 09:49:34 +0330

    add base model

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,9 +1,23 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from typing import Optional
 
 
 router = APIRouter(prefix='/blog', tags=['blog'])
 
 
+
+class BlogModel(BaseModel):
+    pass
+    title:str
+    content:str
+    nb_comment:int
+    publisher:Optional[bool]
+
+
 @router.post('/new')
-def create_blog():
-    return {'message':'OK'}
+def create_blog(blog: BlogModel):
+    return {
+        'message': 'OK',
+        'data': blog,
+    }
