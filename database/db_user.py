7c665c60b898ba8730201a27f1774450e4c42b59
Hossein <hossienbehzadi1381@gmail.com>
2023-12-21 10:53:11 +0330

    adding add user, delete user, update user and get user APIs

--- database/db_user.py
+++ database/db_user.py
@@ -15,3 +15,31 @@ def create_user(db: Session, request: UserBase):
     db.commit()
     db.refresh(user)
     return user
+
+
+def get_all_users(db: Session):
+    return db.query(DbUser).all()
+
+
+
+def get_user(id,db: Session):
+    return db.query(DbUser).filter(DbUser.id == id).first()
+
+
+def delete_user(id, db: Session):
+    user = get_user(id, db)
+    db.delete(user)
+    db.commit()
+    return 'User deleted'
+
+def update_user(id, db:Session, request: UserBase):
+    user = db.query(DbUser).filter(DbUser.id == id)
+    user.update({
+        DbUser.username: request.username,
+        DbUser.email: request.email,
+        DbUser.password: Hash.bcrypt(request.password),
+    })
+    db.commit()
+
+    return "User updated"
+
--- router/user.py
+++ router/user.py
@@ -15,23 +15,34 @@ router = APIRouter(prefix='/user', tags=['user'])
 
 # create user
 
-@router.post('/', response_model=UserDisplay)
+@router.post('/create', response_model=UserDisplay)
 def create_user(user:UserBase, db=Depends(get_db)):
     return db_user.create_user(db, user)
 
 
-# read user
+
+# read all user
+@router.get('/getall', response_model=List[UserDisplay])
+def get_all_users(db=Depends(get_db)):
+    return db_user.get_all_users(db)
 
 
-
+# read one user
+@router.get('/get/{id}', response_model=UserDisplay)
+def get_user(id:int,db=Depends(get_db)):
+    return db_user.get_user(id, db)
 
 
 # update user
-
-
+@router.post('/update/{id}', response_model=None)
+def update_user(id: int, user: UserBase, db=Depends(get_db)):
+    return db_user.update_user(id, db, user)
 
 
 
 
 # delete user
+@router.get('/deleteuser/{id}')
+def delete_user(id: int, db=Depends(get_db)):
+    return db_user.delete_user(id, db)
 
